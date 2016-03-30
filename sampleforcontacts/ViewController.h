f8f7c231c35a1d940fd17face089dfb167cdb81e
Alina Hambaryan <alinah@administrators-imac-2.local>
2016-03-30 11:10:06 +0400

    Initial Commit

---
+++ sampleforcontacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleforcontacts
+//
+//  Created by Alina Hambaryan on 3/30/16.
+//  Copyright © 2016 Alina Hambaryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sampleforcontacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sampleforcontacts
+//
+//  Created by Alina Hambaryan on 3/30/16.
+//  Copyright © 2016 Alina Hambaryan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
